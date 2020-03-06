913ca37609e42f8a5b5b375414453d1c4d0fa2a5
Niraj Tiwari <tniraj7@gmail.com>
2020-03-06 13:38:48 +0900

    Add Course data model

---
+++ ExpandableTableViewCell/ExpandableTableViewCell/Course.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Course: Identifiable {
+    var id = UUID()
+    var image: String
+    var title: String
+    var text: String
+    var date: String
+    var isExpanded: Bool
+}
