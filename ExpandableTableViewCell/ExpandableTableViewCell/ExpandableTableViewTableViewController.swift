557448a3a74574d9db15556114d84fa5892a0d5a
Niraj Tiwari <tniraj7@gmail.com>
2020-03-07 19:49:58 +0900

    modified courseData view model

--- ExpandableTableViewCell/ExpandableTableViewCell/ExpandableTableViewTableViewController.swift
+++ ExpandableTableViewCell/ExpandableTableViewCell/ExpandableTableViewTableViewController.swift
@@ -5,14 +5,21 @@ class ExpandableTableViewTableViewController: UITableViewController {
     var isExapnded: Bool = false
 
     var courseData = [
-        Course(image: "Illustration1", title: "SwiftUI", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 26", isExpanded: false),
-        Course(image: "Illustration2", title: "Framer X", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 11", isExpanded: false),
-        Course(image: "Illustration3", title: "CSS Layout", text: "Learn how to combine CSS Grid, Flexbox, animations and responsive design to create a beautiful prototype in CodePen.", date: "MAY 26", isExpanded: false),
-        Course(image: "Illustration4", title: "React Native Part 2", text: "Learn how to implement gestures, Lottie animations and Firebase login.", date: "MAY 15", isExpanded: false),
-        Course(image: "Certificate1", title: "Unity", text: "Unity course teaching basics, C#, assets, level design and gameplay", date: "MAR 19", isExpanded: false),
-        Course(image: "Certificate2", title: "React Native for Designers", text: "Build your own iOS and Android app with custom animations, Redux and API data.", date: "FEB 14", isExpanded: false),
-        Course(image: "Certificate3", title: "Vue.js", text: "Make a dashboard web-app with a complete login system, dark mode, and animated charts for your data.", date: "JAN 23", isExpanded: false)
-    
+        
+        Course(image: "Illustration1", title: "SwiftUI", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 26", isExpanded: false, chapters: ["1", "2", "3", "4"]),
+        
+        Course(image: "Illustration2", title: "Framer X", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 11", isExpanded: false, chapters: ["1", "2", "3", "4"]),
+        
+        Course(image: "Illustration3", title: "CSS Layout", text: "Learn how to combine CSS Grid, Flexbox, animations and responsive design to create a beautiful prototype in CodePen.", date: "MAY 26", isExpanded: false, chapters: ["1", "2", "3", "4"]),
+        
+        Course(image: "Illustration4", title: "React Native Part 2", text: "Learn how to implement gestures, Lottie animations and Firebase login.", date: "MAY 15", isExpanded: false, chapters: ["1", "2", "3", "4"]),
+        
+        Course(image: "Certificate1", title: "Unity", text: "Unity course teaching basics, C#, assets, level design and gameplay", date: "MAR 19", isExpanded: false, chapters: ["1", "2", "3", "4"]),
+        
+        Course(image: "Certificate2", title: "React Native for Designers", text: "Build your own iOS and Android app with custom animations, Redux and API data.", date: "FEB 14", isExpanded: false, chapters: ["1", "2", "3", "4"]),
+        
+        Course(image: "Certificate3", title: "Vue.js", text: "Make a dashboard web-app with a complete login system, dark mode, and animated charts for your data.", date: "JAN 23", isExpanded: false, chapters: ["1", "2", "3", "4"])
+        
     ]
     
     override func viewDidLoad() {
